aca5c524aca4cfb23a0ca56e9ba1de44aa1ed2af
pguillot <paul.guillot@ensae.fr>
2024-10-16 21:51:37 +0000

    réorganisation du code

--- main.R
+++ main.R
@@ -26,56 +26,44 @@ source("~/work/algosto/resultats.R")
 #devtools::load_all("C:/Users/Paul/Documents/codeRTheseLPSM")
 
 
+n <- 1e4
+
+d <- 10
+
+mu1 <- rep(0,d)
+
+m2 <- 5*rep(1,d)
+
+Sigma1 <- diag(1:10)
+
+
+Sigma2 <- 10*diag(1:10)
+
+
+
+
+p1 <- 1
+
+p2 <- 0
+
+resultsSimul <- genererEchantillon(n,d,mu1,mu2,p1,p2,Sigma1,Sigma2)
+
+
+Z <- resultsSimul$Z
+
 params <- initialiser_parametres(
-  d = 10,
-  q = 10,
-  n = 1e6,
+  Y = Z,
   c = sqrt(10),
-  m0 = rep(0, 10),
-  Sigma2 = diag(1:10),#Tester une matrice de covariance non diagonale
-  Sigma1 =  matrix(c(2, 0.8, 0.5, 0.3, 0.2, 0.1, 0.05, 0.02, 0.01, 0.005,
-                     0.8, 3, 0.7, 0.4, 0.3, 0.2, 0.1, 0.05, 0.02, 0.01,
-                     0.5, 0.7, 1.5, 0.6, 0.5, 0.3, 0.2, 0.1, 0.05, 0.02,
-                     0.3, 0.4, 0.6, 2.5, 0.7, 0.5, 0.3, 0.2, 0.1, 0.05,
-                     0.2, 0.3, 0.5, 0.7, 1.8, 0.8, 0.5, 0.3, 0.2, 0.1,
-                     0.1, 0.2, 0.3, 0.5, 0.8, 2.2, 0.7, 0.5, 0.3, 0.2,
-                     0.05, 0.1, 0.2, 0.3, 0.5, 0.7, 1.7, 0.6, 0.5, 0.3,
-                     0.02, 0.05, 0.1, 0.2, 0.3, 0.5, 0.6, 2.1, 0.7, 0.5,
-                     0.01, 0.02, 0.05, 0.1, 0.2, 0.3, 0.5, 0.7, 1.9, 0.8,
-                     0.005, 0.01, 0.02, 0.05, 0.1, 0.2, 0.3, 0.5, 0.8, 2.5),
-                   nrow = 10, ncol = 10, byrow = TRUE),
-  #Sigma3 = 10 * diag((1:10)),
-  p1 = 1,
-  p2 = 0,
-  
-  mu1 = rep(0,10 ),
-  mu2 = 5 * rep(1, 10),
   r = 1,
   k = 1
 )
-params$n
-
-
-# Creation d'une matrice de covariance
-A <- matrix(runif(100, min=-1, max=1), nrow=10)
-A <- (A + t(A)) / 2 
-cov_matrix <- A %*% t(A)
-
-cov_matrix
 
 #eigen(resultsSimul$Vvrai)$vectors
 
-resultsSimul <- genererEchantillon(params$n,params$d,params$mu1,params$mu2,params$p1,params$p2,cov_matrix,params$Sigma2)
-
 #resultsSimul$labelsVrais
-
-Z <- resultsSimul$Z
 #Z
 results <- estimMVOutliers(Z,params$c ,params$n,params$d,params$d,params$r,aa = 0.75)
 
-
-
-  
 resultsStr <- streaming(Z,params$n/params$k,params$k,params$c,params$n,params$d,params$q,params$r)
 
 #lambdaResultat <- RobbinsMC2(1e4,results$vpMCM[99,],10^(-8),alpha = 0.75,c= 2,w=2,samp=1e4, initbarre = results$vpMCM[99,],cbarre=0)
@@ -86,7 +74,7 @@ resultsStr <- streaming(Z,params$n/params$k,params$k,params$c,params$n,params$d,
 
 #Affichage de l'estimation des erreurs pour l'estimation de mbarre
 
-affiche_erreurs_mediane_geometrique(calculErreursM(results$miter,params$m,params$n/params$k),params$n/params$k)
+affiche_erreurs_mediane_geometrique(calculErreursM(results$miter,rep(0,d),params$n/params$k),params$n/params$k)
 
 #Affichage de l'estimation des erreurs pour l'estimation de Vbarre
 
@@ -100,11 +88,10 @@ affiche_erreurs_vp_mcm(calculErreursVpMCM(results$vpMCM,resultsSimul$VpvraiesV,p
 
 #Affichage de l'erreur d'estimation des vecteurs propres de la MCM
 
-affiche_erreurs_vectp_mcm(calculErreursVectpMCM(results$U,eigen(cov_matrix)$vectors,params$n/params$k,params$d),params$n/params$k,params$c)
+affiche_erreurs_vectp_mcm(calculErreursVectpMCM(results$U,eigen(params$Sigma)$vectors,params$n/params$k,params$d),params$n/params$k,params$c)
 #resultsSimul$VcovVrai
 #Affichage de l'erreur d'estimation des valeurs propres de la matrice de covariance
-affiche_erreurs_cov(calculErreursValPropreCov(results$lambdaIter,eigen(cov_matrix)$values,params$n/params$k),params$n/params$k)
-results$U[999,,] 
+affiche_erreurs_cov(calculErreursValPropreCov(results$lambdaIter,eigen(params$Sigma)$values,params$n/params$k),params$n/params$k)
 #Tracer avec les vraies valeurs 
 
 #Comparaison stat khi-deux avec densité théorique Khi deux
@@ -115,16 +102,17 @@ table_contingence(resultsSimul$labelsVrais[1:999999],results$outlier_labels)
 
 #calculErreursSigma(params$Sigma1,results$U,resultsStr$lambdatilde,params$n,params$d)
 
-affiche_erreurs_Sigma(calculErreursSigma(cov_matrix,results$U,results$lambdatilde,params$n/params$k,params$d),params$n/params$k,params$c)
+affiche_erreurs_Sigma(calculErreursSigma(params$Sigma,results$U,results$lambdatilde,params$n/params$k,params$d),params$n/params$k,params$c)
 results$U[999999,,] <- results$U[999999,,] %*% diag(1/sqrt(colSums(results$U[999999,,]^2)))
 results$U[999999,,] %*% diag(1/sqrt(colSums(results$U[999999,,]^2)))
 SigmaEstim <- t(results$U[999999,,])%*%diag(results$lambdaIter[999999,])%*%results$U[999999,,]
 
-plot(cov_matrix,SigmaEstim) 
+plot(params$Sigma,SigmaEstim) 
 abline(0,1)
 
-statTheorique <- calculeStatChi2(resultsSimul$Z,params$Sigma1)
+statTheorique <- calculeStatChi2(resultsSimul$Z,params$Sigma)
 
 densiteHistKhi2(statTheorique,params$d)
+
 
 
--- parametres.R
+++ parametres.R
@@ -19,20 +19,15 @@ library("dplyr")
 
 
 
-initialiser_parametres <- function(d,q,n,c,m0,Sigma1,Sigma2,p1,p2,mu1,mu2,r,k) {
+initialiser_parametres <- function(Y,c,r,k) {
   # Initialisation des paramètres
   params <- list(
-    d = d   ,              # Dimension des données mettre ncol(Y)
-    n = n,             # Taille de l'échantillon nrow(Y)
-    q = q,              # Nombre de plus grandes valeurs propres
+    d = ncol(Y)   ,              # Dimension des données mettre ncol(Y)
+    n = nrow(Y),# Taille de l'échantillon nrow(Y)
+    Sigma = cov(Y),
+    q = d,              # Nombre de plus grandes valeurs propres
     c = c,        # Taux d'apprentissage
-    m = m0,      # Moyenne réelle (vecteur de zéros)
-    Sigma1 = Sigma1,        # Covariance des non outliers
-    Sigma2 = Sigma2,    # Covariance des outliers
-    p1 = p1,            # Proportion de non outliers
-    p2 = p2,            # Proportion d'outliers
-    mu1 = mu1,    # Moyenne des non outliers
-    mu2 = mu2, #a Moyenne des outliers
+    #m = m0,      # Moyenne réelle (vecteur de zéros)
     r = r, #Paramètre r pour générer les variables aléatoires
     k = k #Taille des blocs
       )
