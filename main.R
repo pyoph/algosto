34e8a7655a79d5e5af26fedac4887c820d16737f
pyoph <115776656+pyoph@users.noreply.github.com>
2025-04-07 00:24:07 +0200

    ajout code boxplots

--- main.R
+++ main.R
@@ -230,191 +230,191 @@ plot2 = ggplot(df_long, aes(x = taux_contamination, y = Valeur,
   theme(legend.position = "top")  # Légende en haut
 
 plot1+plot2
-save(results_metrics,file = "results_metricsMaronnaZamar.Rdata")
-
-
-
-
-# latex_table_results_metrics <- xtable(results_metrics)
-
-results_without_RMSE_Med <- results_metrics %>%
-     select(-contains("RMSE_Med")) %>%  # Supprimer toutes les colonnes RMSE_Med
-     select(-contains("Cov"))
-
-latex_table <- kable(results_without_RMSE_Med, format = "latex", caption = "Résultats contamination en moyenne 20 runs", label = "tab:results")
-writeLines(latex_table, "resultats_contamination_zero.tex")
-
-latex_table_results_metrics_without_RMSE_Med <- xtable(results_without_RMSE_Med)
-
-
-  
-#Création d'une liste vide 
-liste_resultats_outliers <- list()
-
-for (i in 1:nbruns)
-{
-  resultats_outliers <- calcule_outliers(contamin = "uniforme")
-  liste_resultats_outliers[[i]] <- resultats_outliers 
-}
-
-#resultats_outliers <- round(resultats_outliers,2)
-
-
-# Calculer la moyenne de chaque colonne sur tous les dataframes
-moyenne_resultats <- round(Reduce("+", liste_resultats_outliers) / nbruns,2)
-
-# Afficher la moyenne
-print(moyenne_resultats)
-
-save(moyenne_resultats,file = "outliersUnifhypercubeunite.Rdata")
-
-moyenne_resultatsEnreg <- moyenne_resultats  %>% select(-FN_Cov, -FP_Cov, -Tps_Cov)
-
-latex_table <- xtable(moyenne_resultatsEnreg)
-
-
-taux_contamination <- c(0,2, 5, 10, 15, 20, 25, 30, 40)
-
-###Test fonction d'estimation online affichage des boxplots des erreurs
-pdf("Resultats_Erreurs_Sigma Toeplitzvar1sqrtd.pdf", width = 10, height = 7)
-
-
-#Calcul des erreurs pour tracer les boxplots
-
-erreursSigmaBoxplotOnline <-array(0, dim = c(nbruns,length(taux_contamination),n))
-
-erreursSigmaBoxplotStreaming <- array(0, dim = c(nbruns,length(taux_contamination),n))
-
-erreursVarOracle <- matrix(0,nbruns,length(taux_contamination))
-
-#Somme des erreurs online et streaming pour les moyenner ensuite
-
-somme_erreursOnline <- matrix(0,n,length(taux_contamination))
-somme_erreursStreaming <- matrix(0,n,length(taux_contamination))
-
-
-depart = 100
-
-for (m in (1:nbruns)){
-for (i in seq_along(taux_contamination)) 
-{
-  
-
-  contamin = "moyenne"
-  #Initialisation des erreurs online et streaming
-  erreursonline <- rep(0,n)
-  erreursStr <- rep(0,n)
-  delta <- taux_contamination[i]
-  #delta <- 0
-  
-  #Génération des échantillons
-  p1 <- 1 - delta / 100
-  
-  p2 <- 1 - p1
-  
-  resultsSimul <- genererEchantillon(n, d, mu1, mu2, p1, p2, Sigma1 = Sigma1, Sigma2 = Sigma2,contamin = contamin)
-  Z <- resultsSimul$Z
-  
-  
-  #Estimation online
-  #results <- estimMV(Z,Vinit = Sigma1,methode = "eigen")
-  
-  resultatsOnline <- estimation(Z,depart, methodeEstimation = "online")
-  SigmaEstimOnline <- resultatsOnline$SigmaOnline
-  distances <- resultatsOnline$distances
-  miterOn <- resultatsOnline$miter
-  
-  #Estimation offline
-  resultsOffline <- estimation(Z,depart, methodeEstimation = "offline")
-  SigmaEstimOffline <- resultsOffline$SigmaOffline
-  
-  #Estimation en streaming 
-  resultsStr <- estimation(Z, depart, methodeEstimation = "streaming")
-  miterStr <- estimation(Z, depart, methodeEstimation = "streaming")$miter
-  SigmaStr <- resultsStr$SigmaStreaming
-  
-  
-  #calcul des erreurs à chaque itération
-  for (k in (1: n)){
-  erreursonline[k] <- calculErreursNormeFrobenius(SigmaEstimOnline[k,,],Sigma1)
-  
-  erreursStr[k] <- calculErreursNormeFrobenius(SigmaStr[k,,],Sigma1)
-  }
-  erreursSigmaBoxplotOnline[m,i,] <- erreursonline
-  erreursSigmaBoxplotStreaming[m,i,] <- erreursStr
-  erreursVarOracle[m,i] <- EstimVarMC(nbiter = 1e2, delta = delta, Sigma = Sigma1)
-  
-  #erreursoffline <- calculErreursNormeFrobenius(SigmaEstimOffline,Sigma1)
-  #affiche_erreursSigma(erreurs_online = erreursonline, contamination = delta)
-  
-  #affiche_erreursSigma(erreurs_online = erreursStr, contamination = delta)
-  #Stockage des erreurs :
-  print(somme_erreursOnline[1e4,i])
-  somme_erreursOnline[,i] <- somme_erreursOnline[,i] + erreursonline
-  print(somme_erreursOnline[1e4,i])
-  somme_erreursStreaming[,i] <- somme_erreursStreaming[,i] + erreursStr
- #Affichage des SigmaEstim Online, Offline et théorique
- 
-#plot_comparaison_sigma(Sigma1, SigmaEstimOffline = SigmaEstimOffline,SigmaEstimOnline = SigmaEstimOnline,delta)
-
- 
-  }
- #points(Sigma1, SigmaEstimOffline, col=4); abline(0, 1)
- 
-  
-}
-#Calcul des moyennes de erreurs
-
-moy_erreursSigmaBoxplotOnline <- round(apply(erreursSigmaBoxplotOnline, c(2,3), mean), 2)
-moy_erreursSigmaBoxplotStreaming <- round(apply(erreursSigmaBoxplotStreaming, c(2,3), mean), 2)
-
-moy_erreursVarOracle <- round(apply(erreursVarOracle, 2, mean), 2)
-
-creer_boxplot_erreurs(moy_erreursSigmaBoxplotOnline,taux_contamination,methode= "online",erreursVarOracle[1,])
-creer_boxplot_erreurs(moy_erreursSigmaBoxplotStreaming,taux_contamination,methode= "streaming",erreursVarOracle[1,])
-
-
-dev.off()
-
-#Calcul des moyennes des erreurs 
-
-# Calcul de la moyenne des erreurs par taux de contamination
-moy_erreursOnline <- (somme_erreursOnline[100:1e4,])/nbruns
-moy_erreursStreaming <-(somme_erreursStreaming[100:1e4,])/nbruns
-
-# Créer une liste pour stocker les graphiques
-list_plots <- list()
-# Nombre d'itérations à considérer
-iterations <- seq(1, 9900)  
-for (i in seq_along(taux_contamination)) 
-{
-  delta <- taux_contamination[i]
-# Transformer les données en format long pour ggplot
-df_erreurs <- data.frame(
-  Iteration = rep(iterations, 2),
-  Erreur_Moyenne = c(moy_erreursOnline[iterations,i], moy_erreursStreaming[iterations,i]),
-  Méthode = rep(c("Online", "Streaming"), each = length(iterations))
-)
-
-# Tracer les courbes
-p <- ggplot(df_erreurs, aes(x = Iteration, y = Erreur_Moyenne, color = Méthode)) +
-  geom_line(size = 1.2) +  # Ligne plus épaisse
-  geom_point(size = 2) +   # Points pour les valeurs précises
-  scale_x_log10() +        # Échelle logarithmique pour les itérations
-  labs(title = paste("taux de contamination =  ",delta," %"),
-       x = "Nombre d'itérations",
-       y = "Erreur Moyenne (norme de Frobenius)") +
-  theme_minimal() +
-  theme(legend.position = "top")
-print(p)
-list_plots[[i]] <- p
-}
-
-grid.arrange(grobs = list_plots[1:4], ncol = 2, nrow = 2)
-grid.arrange(grobs = list_plots[5:9], ncol = 3, nrow = 3)
-
-
-
+# save(results_metrics,file = "results_metricsMaronnaZamar.Rdata")
+# 
+# 
+# 
+# 
+# # latex_table_results_metrics <- xtable(results_metrics)
+# 
+# results_without_RMSE_Med <- results_metrics %>%
+#      select(-contains("RMSE_Med")) %>%  # Supprimer toutes les colonnes RMSE_Med
+#      select(-contains("Cov"))
+# 
+# latex_table <- kable(results_without_RMSE_Med, format = "latex", caption = "Résultats contamination en moyenne 20 runs", label = "tab:results")
+# writeLines(latex_table, "resultats_contamination_zero.tex")
+# 
+# latex_table_results_metrics_without_RMSE_Med <- xtable(results_without_RMSE_Med)
+# 
+# 
+#   
+# #Création d'une liste vide 
+# liste_resultats_outliers <- list()
+# 
+# for (i in 1:nbruns)
+# {
+#   resultats_outliers <- calcule_outliers(contamin = "uniforme")
+#   liste_resultats_outliers[[i]] <- resultats_outliers 
+# }
+# 
+# #resultats_outliers <- round(resultats_outliers,2)
+# 
+# 
+# # Calculer la moyenne de chaque colonne sur tous les dataframes
+# moyenne_resultats <- round(Reduce("+", liste_resultats_outliers) / nbruns,2)
+# 
+# # Afficher la moyenne
+# print(moyenne_resultats)
+# 
+# save(moyenne_resultats,file = "outliersUnifhypercubeunite.Rdata")
+# 
+# moyenne_resultatsEnreg <- moyenne_resultats  %>% select(-FN_Cov, -FP_Cov, -Tps_Cov)
+# 
+# latex_table <- xtable(moyenne_resultatsEnreg)
+# 
+# 
+# taux_contamination <- c(0,2, 5, 10, 15, 20, 25, 30, 40)
+# 
+# ###Test fonction d'estimation online affichage des boxplots des erreurs
+# pdf("Resultats_Erreurs_Sigma Toeplitzvar1sqrtd.pdf", width = 10, height = 7)
+# 
+# 
+# #Calcul des erreurs pour tracer les boxplots
+# 
+# erreursSigmaBoxplotOnline <-array(0, dim = c(nbruns,length(taux_contamination),n))
+# 
+# erreursSigmaBoxplotStreaming <- array(0, dim = c(nbruns,length(taux_contamination),n))
+# 
+# erreursVarOracle <- matrix(0,nbruns,length(taux_contamination))
+# 
+# #Somme des erreurs online et streaming pour les moyenner ensuite
+# 
+# somme_erreursOnline <- matrix(0,n,length(taux_contamination))
+# somme_erreursStreaming <- matrix(0,n,length(taux_contamination))
+# 
+# 
+# depart = 100
+# 
+# for (m in (1:nbruns)){
+# for (i in seq_along(taux_contamination)) 
+# {
+#   
+# 
+#   contamin = "moyenne"
+#   #Initialisation des erreurs online et streaming
+#   erreursonline <- rep(0,n)
+#   erreursStr <- rep(0,n)
+#   delta <- taux_contamination[i]
+#   #delta <- 0
+#   
+#   #Génération des échantillons
+#   p1 <- 1 - delta / 100
+#   
+#   p2 <- 1 - p1
+#   
+#   resultsSimul <- genererEchantillon(n, d, mu1, mu2, p1, p2, Sigma1 = Sigma1, Sigma2 = Sigma2,contamin = contamin)
+#   Z <- resultsSimul$Z
+#   
+#   
+#   #Estimation online
+#   #results <- estimMV(Z,Vinit = Sigma1,methode = "eigen")
+#   
+#   resultatsOnline <- estimation(Z,depart, methodeEstimation = "online")
+#   SigmaEstimOnline <- resultatsOnline$SigmaOnline
+#   distances <- resultatsOnline$distances
+#   miterOn <- resultatsOnline$miter
+#   
+#   #Estimation offline
+#   resultsOffline <- estimation(Z,depart, methodeEstimation = "offline")
+#   SigmaEstimOffline <- resultsOffline$SigmaOffline
+#   
+#   #Estimation en streaming 
+#   resultsStr <- estimation(Z, depart, methodeEstimation = "streaming")
+#   miterStr <- estimation(Z, depart, methodeEstimation = "streaming")$miter
+#   SigmaStr <- resultsStr$SigmaStreaming
+#   
+#   
+#   #calcul des erreurs à chaque itération
+#   for (k in (1: n)){
+#   erreursonline[k] <- calculErreursNormeFrobenius(SigmaEstimOnline[k,,],Sigma1)
+#   
+#   erreursStr[k] <- calculErreursNormeFrobenius(SigmaStr[k,,],Sigma1)
+#   }
+#   erreursSigmaBoxplotOnline[m,i,] <- erreursonline
+#   erreursSigmaBoxplotStreaming[m,i,] <- erreursStr
+#   erreursVarOracle[m,i] <- EstimVarMC(nbiter = 1e2, delta = delta, Sigma = Sigma1)
+#   
+#   #erreursoffline <- calculErreursNormeFrobenius(SigmaEstimOffline,Sigma1)
+#   #affiche_erreursSigma(erreurs_online = erreursonline, contamination = delta)
+#   
+#   #affiche_erreursSigma(erreurs_online = erreursStr, contamination = delta)
+#   #Stockage des erreurs :
+#   print(somme_erreursOnline[1e4,i])
+#   somme_erreursOnline[,i] <- somme_erreursOnline[,i] + erreursonline
+#   print(somme_erreursOnline[1e4,i])
+#   somme_erreursStreaming[,i] <- somme_erreursStreaming[,i] + erreursStr
+#  #Affichage des SigmaEstim Online, Offline et théorique
+#  
+# #plot_comparaison_sigma(Sigma1, SigmaEstimOffline = SigmaEstimOffline,SigmaEstimOnline = SigmaEstimOnline,delta)
+# 
+#  
+#   }
+#  #points(Sigma1, SigmaEstimOffline, col=4); abline(0, 1)
+#  
+#   
+# }
+# #Calcul des moyennes de erreurs
+# 
+# moy_erreursSigmaBoxplotOnline <- round(apply(erreursSigmaBoxplotOnline, c(2,3), mean), 2)
+# moy_erreursSigmaBoxplotStreaming <- round(apply(erreursSigmaBoxplotStreaming, c(2,3), mean), 2)
+# 
+# moy_erreursVarOracle <- round(apply(erreursVarOracle, 2, mean), 2)
+# 
+# creer_boxplot_erreurs(moy_erreursSigmaBoxplotOnline,taux_contamination,methode= "online",erreursVarOracle[1,])
+# creer_boxplot_erreurs(moy_erreursSigmaBoxplotStreaming,taux_contamination,methode= "streaming",erreursVarOracle[1,])
+# 
+# 
+# dev.off()
+# 
+# #Calcul des moyennes des erreurs 
+# 
+# # Calcul de la moyenne des erreurs par taux de contamination
+# moy_erreursOnline <- (somme_erreursOnline[100:1e4,])/nbruns
+# moy_erreursStreaming <-(somme_erreursStreaming[100:1e4,])/nbruns
+# 
+# # Créer une liste pour stocker les graphiques
+# list_plots <- list()
+# # Nombre d'itérations à considérer
+# iterations <- seq(1, 9900)  
+# for (i in seq_along(taux_contamination)) 
+# {
+#   delta <- taux_contamination[i]
+# # Transformer les données en format long pour ggplot
+# df_erreurs <- data.frame(
+#   Iteration = rep(iterations, 2),
+#   Erreur_Moyenne = c(moy_erreursOnline[iterations,i], moy_erreursStreaming[iterations,i]),
+#   Méthode = rep(c("Online", "Streaming"), each = length(iterations))
+# )
+# 
+# # Tracer les courbes
+# p <- ggplot(df_erreurs, aes(x = Iteration, y = Erreur_Moyenne, color = Méthode)) +
+#   geom_line(size = 1.2) +  # Ligne plus épaisse
+#   geom_point(size = 2) +   # Points pour les valeurs précises
+#   scale_x_log10() +        # Échelle logarithmique pour les itérations
+#   labs(title = paste("taux de contamination =  ",delta," %"),
+#        x = "Nombre d'itérations",
+#        y = "Erreur Moyenne (norme de Frobenius)") +
+#   theme_minimal() +
+#   theme(legend.position = "top")
+# print(p)
+# list_plots[[i]] <- p
+# }
+# 
+# grid.arrange(grobs = list_plots[1:4], ncol = 2, nrow = 2)
+# grid.arrange(grobs = list_plots[5:9], ncol = 3, nrow = 3)
+# 
+# 
+# 
 
 # 
 # ###Calcul du meilleur AUC pour chaque méthode
@@ -480,12 +480,15 @@ grid.arrange(grobs = list_plots[5:9], ncol = 3, nrow = 3)
 # Dimensions : [100, 9, 8]
 rmse_data <- data.frame()
 
+rmseSigmaBP = results_metrics$rmseSigmaBP
+
 # Noms des méthodes
 methods <- c("Cov", "OGK", "Comed", "Shrink", 
              "Online", "Offline", "Streaming", "FastMCD")
 
 # Noms des taux de contamination (à adapter si tu as les vrais)
-taux_contamination <- paste0("Taux_", 1:9)
+#taux_contamination <- paste0("Taux_", 1:9)
+taux_contamination <- c(0, 2, 5, 10, 15, 20, 25, 30, 40)
 
 for (m in 1:8) {
   for (c in 1:9) {
@@ -498,17 +501,13 @@ for (m in 1:8) {
     rmse_data <- rbind(rmse_data, temp_df)
   }
 }
-
-# 🎨 Plot : facettes par méthode, boxplots par taux
-library(ggplot2)
-
-ggplot(rmse_data, aes(x = Taux, y = RMSE)) +
+ggplot(rmse_data, aes(x = factor(Taux), y = RMSE)) +
   geom_boxplot(fill = "lightblue", color = "darkblue") +
   facet_wrap(~ Méthode, scales = "free_y") +
   theme_minimal(base_size = 14) +
   labs(
-    title = "RMSE par Taux de Contamination et Méthode",
-    x = "Taux de contamination",
-    y = "RMSE"
+    title = "Boxplot of Frobenius norm error",
+    x = "Contamination rate",
+    y = "Frobenius norm"
   ) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
