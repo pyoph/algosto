6d92ea6fe245a56921d46451561d42df912625b1
pyoph <paul.guillot2@gmail.com>
2025-06-13 15:57:17 +0200

    code

--- SR/ComputKLgauss.R
+++ SR/ComputKLgauss.R
@@ -80,7 +80,7 @@ for (j in seq_along(contamin_rate))
   print(paste("k ",k))
   k = k1grid[i]
 
-    data <- genererEchantillon(n,d,mu1,mu2 = k*mu1,p1 = 1- r/100,r/100,Sigma1,Sigma2,contamin = "moyenne",cluster = FALSE)
+    data <- genererEchantillon(n,d,mu1,mu2 = k*mu1,p1 = 1- r/100,r/100,Sigma1,Sigma2 = Sigma1,contamin = "moyenne",cluster = FALSE)
   Z = data$Z
   compt = 1
 for (m in methodes)  
@@ -123,4 +123,81 @@ for (m in methodes)
 
 }
 
+
+plot(k1grid,faux_positifs[,1,])
+
+fp_seuil =array(0,dim = c(length(contamin_rate), length(methodes)))
+
+for (k in seq_along(contamin_rate))
+{
+ r = 0 
+ compt = 1
+  data <- genererEchantillon(n,d,mu1,mu2 = k*mu1,p1 = 1- r/100,r/100,Sigma1,Sigma2 = Sigma1,contamin = "moyenne",cluster = FALSE)
+  Z = data$Z
+  for (m in methodes){
+  if(m == "online"){
+    print (m)
+    resultats = StreamingOutlierDetection(Z,batch = 1)
+    Sigma = resultats$Sigma[nrow(Z),,]
+    outliers = resultats$outlier_labels
+    cutoffCorr = qchisq(.95,df = d)*median(resultats$distances)/qchisq(.5,df = d)
+    resultats = StreamingOutlierDetection(Z,batch = 1,cutoff = cutoffCorr)
+    outliers = resultats$outlier_labels
+    
+  }
+  if(m == "streaming"){
+    print (m)
+    resultats = StreamingOutlierDetection(Z,batch = ncol(Z))
+    Sigma = resultats$Sigma[nrow(Z),,]
+    outliers = resultats$outlier_labels
+    cutoffCorr = qchisq(.95,df = d)*median(resultats$distances)/qchisq(.5,df = d)
+    resultats = StreamingOutlierDetection(Z,batch = 1,cutoff = cutoffCorr)
+    outliers = resultats$outlier_labels
+    
+    
+  }
+  
+  
+  tc <- table(data$labelsVrais[1:(nrow(Z))], as.numeric(outliers)[1:(nrow(Z))])
+  if ("0" %in% rownames(tc)){
+    if((tc["0","0"] + tc["0","1"]) != 0)
+    {fp_seuil[k,compt]   <-(tc["0", "1"]/(tc["0", "1"] + tc["0", "0"]))*100
+    print(fp_seuil[k,compt])}
+  }
+  if ("1" %in% rownames(tc)){
+    if((tc["1","0"] + tc["1","1"]) != 0)
+    {fp_seuil[k,compt]   <-(tc["1", "1"]/(tc["1", "1"] + tc["1", "0"]))*100
+    print(fp_seuil[k,compt])}}
+  compt = compt + 1}
+}  
+  
+
+
+
+# Paramètres
+alpha <- 0.05
+conf_level <- 1 - alpha
+p_seq <- seq(0.6, 1, length.out = 200)
+N <- nrow(Z)  # N doit être défini au préalable
+
+# Calcul des n0 et des x théoriques
+n0 <- floor(p_seq * N)
+x <- round(n0 * alpha)
+
+# Calcul de l’intervalle de confiance exact (Clopper-Pearson)
+conf <- binom.confint(x = x, n = n0, conf.level = conf_level, methods = "exact")
+
+
+# Tracé des bornes
+plot(p_seq, conf$lower, type = "l", col = "blue", lwd = 2,
+     ylim = c(0, 0.15), xlab = "p (proportion pour n0 = p * N)",
+     ylab = "Intervalle de confiance de la proportion",
+     main = "IC à 95% pour Bin(n0, alpha = 0.05)")
+lines(p_seq, conf$upper, col = "red", lwd = 2)
+
+fp_positions = c(1.00, 0.95, 0.9,  0.85, 0.80, 0.75, 0.70, 0.65,0.60,0.55)
+
+fp_streaming_values = fp_seuil[2,]
+
+points(fp_offline_positions, fp_online_values_corr, col = "darkgreen", pch = 19)
 
