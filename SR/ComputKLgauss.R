0748d5b324638b215b13994eb5f2db07dabeeaf4
pyoph <115776656+pyoph@users.noreply.github.com>
2025-06-15 01:05:17 +0200

    codes

--- SR/ComputKLgauss.R
+++ SR/ComputKLgauss.R
@@ -10,6 +10,40 @@ KL <- function(parms1, parms2){
          t(parms2$mu-parms1$mu)%*%invSigma2%*%(parms2$mu-parms1$mu))[1, 1]
 }
 
+FrobeniusNormError = function(Sigmahat,Sigma1){
+  return (norm(Sigmahat - Sigma1,"F"))
+}
+
+
+taux_detection <- function(vrais_labels, labels_predits) {
+  # S'assurer que les entrées sont binaires
+  if (!all(vrais_labels %in% c(0, 1)) || !all(labels_predits %in% c(0, 1))) {
+    stop("Les vecteurs doivent contenir uniquement 0 (normal) ou 1 (outlier).")
+  }
+  
+  # Calcul des éléments de la matrice de confusion
+  VP <- sum(vrais_labels == 1 & labels_predits == 1)  # Vrai Positifs
+  FP <- sum(vrais_labels == 0 & labels_predits == 1)  # Faux Positifs
+  FN <- sum(vrais_labels == 1 & labels_predits == 0)  # Faux Négatifs
+  VN <- sum(vrais_labels == 0 & labels_predits == 0)  # Vrai Négatifs
+  
+  # Taux
+  TPR <- if ((VP + FN) > 0) VP / (VP + FN) else NA  # Sensibilité
+  FPR <- if ((FP + VN) > 0) FP / (FP + VN) else NA  # 1 - Spécificité
+  
+  return(list(
+    TPR = TPR,
+    FPR = FPR,
+    VP = VP,
+    FP = FP,
+    FN = FN,
+    VN = VN
+  ))
+}
+
+
+
+
 # Contamination parms: F1
 ParmsF1 <- function(m1, k1, l1, rho1){
   d <- length(m1)
@@ -62,9 +96,7 @@ image.plot(log10(l1grid), rho1grid, log10(KLrho1l1))
 #Tests effets sur norme de Frobenius variation k
 #############################
 
-contamin_rate = seq(0,40,5)
 
-methodes = c("streaming","online","offline")
 
 erreurNormeFrobenius = array(0, dim = c(length(contamin_rate), length(methodes),length(k1grid)))
 faux_positifs = array(0, dim = c(length(contamin_rate), length(methodes),length(k1grid)))
@@ -77,10 +109,10 @@ for (j in seq_along(contamin_rate))
   
   r = contamin_rate[j]
   print(paste("r ", r))
-  print(paste("k ",k))
   k = k1grid[i]
-
-    data <- genererEchantillon(n,d,mu1,mu2 = k*mu1,p1 = 1- r/100,r/100,Sigma1,Sigma2 = Sigma1,contamin = "moyenne",cluster = FALSE)
+  print(paste("k ",k))
+  
+  data <- genererEchantillon(n,d,mu1,mu2 = k*m1,p1 = 1- r/100,r/100,Sigma1,Sigma2 = Sigma1,contamin = "moyenne",cluster = FALSE)
   Z = data$Z
   compt = 1
 for (m in methodes)  
@@ -88,7 +120,7 @@ for (m in methodes)
   
   if(m == "online"){
     print (m)
-    resultats = StreamingOutlierDetection(Z,batch = 1)
+  resultats = StreamingOutlierDetection(Z,batch = 1)
   Sigma = resultats$Sigma[nrow(Z),,]
   outliers = resultats$outlier_labels
   }
@@ -126,6 +158,140 @@ for (m in methodes)
 
 plot(k1grid,faux_positifs[,1,])
 
+###########################################
+#Variation de l1grid
+###########################################
+
+
+erreurNormeFrobeniusL1 = array(0, dim = c(length(contamin_rate), length(methodes),length(l1grid)))
+faux_positifsL1 = array(0, dim = c(length(contamin_rate), length(methodes),length(l1grid)))
+faux_negatifsL1 = array(0, dim = c(length(contamin_rate), length(methodes),length(l1grid)))
+
+
+
+
+for (i in seq_along(l1grid))
+{
+  for (j in seq_along(contamin_rate))
+  {
+    
+    r = contamin_rate[j]
+    print(paste("r ", r))
+    k = l1grid[i]
+    print(paste("k ",k))
+    
+    data <- genererEchantillon(n,d,mu1,mu2 = mu1,p1 = 1- r/100,r/100,Sigma1,Sigma2 = k*Sigma1,contamin = "variance",cluster = FALSE)
+    Z = data$Z
+    compt = 1
+    for (m in methodes)  
+    {
+      
+      if(m == "online"){
+        print (m)
+        resultats = StreamingOutlierDetection(Z,batch = 1)
+        Sigma = resultats$Sigma[nrow(Z),,]
+        outliers = resultats$outlier_labels
+      }
+      if(m == "streaming"){
+        print (m)
+        resultats = StreamingOutlierDetection(Z,batch = ncol(Z))
+        Sigma = resultats$Sigma[nrow(Z),,]
+        outliers = resultats$outlier_labels
+      }
+      
+      
+      print(paste("i = ",i))
+      print(paste("compt = ",compt))
+      print(paste("j = ",j))
+      #print("m = ",m)
+      erreurNormeFrobeniusL1[j,compt,i] = norm(Sigma - Sigma1,"F")  
+      
+      
+      
+      tc <- table(data$labelsVrais[1:(nrow(Z))], as.numeric(outliers)[1:(nrow(Z))])
+      if ("0" %in% rownames(tc)){
+        if((tc["0","0"] + tc["0","1"]) != 0)
+        {faux_positifsL1[j,compt,i]   <-(tc["0", "1"]/(tc["0", "1"] + tc["0", "0"]))*100}
+      }
+      if ("1" %in% rownames(tc)){
+        if((tc["1","0"] + tc["1","1"]) != 0)
+        {faux_negatifsL1[j,compt,i]   <-(tc["1", "1"]/(tc["1", "1"] + tc["1", "0"]))*100}}
+      compt = compt + 1
+    } 
+    
+  }
+  
+}
+
+
+
+###########################################
+#Variation de rho1grid
+###########################################
+
+
+erreurNormeFrobeniusRho = array(0, dim = c(length(contamin_rate), length(methodes),length(rho1grid)))
+faux_positifsRho = array(0, dim = c(length(contamin_rate), length(methodes),length(rho1grid)))
+faux_negatifsRho = array(0, dim = c(length(contamin_rate), length(methodes),length(rho1grid)))
+
+
+
+for (i in seq_along(rho1grid))
+{
+  for (j in seq_along(contamin_rate))
+  {
+    
+    r = contamin_rate[j]
+    print(paste("r ", r))
+    k = rho1grid[i]
+    print(paste("k ",k))
+    Sigma1 <- diag(sqrt(sigmaSq0)) %*% toeplitz(k^(0:(d-1))) %*% diag(sqrt(sigmaSq0))
+    data <- genererEchantillon(n,d,mu1,mu2 = mu1,p1 = 1- r/100,r/100,Sigma1,Sigma2 = Sigma1,contamin = "moyenne",cluster = FALSE)
+    Z = data$Z
+    compt = 1
+    for (m in methodes)  
+    {
+      
+      if(m == "online"){
+        print (m)
+        resultats = StreamingOutlierDetection(Z,batch = 1)
+        Sigma = resultats$Sigma[nrow(Z),,]
+        outliers = resultats$outlier_labels
+      }
+      if(m == "streaming"){
+        print (m)
+        resultats = StreamingOutlierDetection(Z,batch = ncol(Z))
+        Sigma = resultats$Sigma[nrow(Z),,]
+        outliers = resultats$outlier_labels
+      }
+      
+      
+      print(paste("i = ",i))
+      print(paste("compt = ",compt))
+      print(paste("j = ",j))
+      #print("m = ",m)
+      erreurNormeFrobeniusRho[j,compt,i] = norm(Sigma - Sigma1,"F")  
+      
+      
+      
+      tc <- table(data$labelsVrais[1:(nrow(Z))], as.numeric(outliers)[1:(nrow(Z))])
+      if ("0" %in% rownames(tc)){
+        if((tc["0","0"] + tc["0","1"]) != 0)
+        {faux_positifsRho[j,compt,i]   <-(tc["0", "1"]/(tc["0", "1"] + tc["0", "0"]))*100}
+      }
+      if ("1" %in% rownames(tc)){
+        if((tc["1","0"] + tc["1","1"]) != 0)
+        {faux_negatifsRho[j,compt,i]   <-(tc["1", "1"]/(tc["1", "1"] + tc["1", "0"]))*100}}
+      compt = compt + 1
+    } 
+    
+  }
+  
+}
+
+
+
+
 calcule_fp = function(corr = TRUE){
 
   fp_seuil =array(0,dim = c(length(contamin_rate), length(methodes)))
@@ -137,6 +303,9 @@ for (k in seq_along(contamin_rate))
   data <- genererEchantillon(n,d,mu1,mu2 = k*mu1,p1 = 1- r/100,r/100,Sigma1,Sigma2 = Sigma1,contamin = "moyenne",cluster = FALSE)
   Z = data$Z
   for (m in methodes){
+    
+    
+    
     
     if(m == "offline"){
       
