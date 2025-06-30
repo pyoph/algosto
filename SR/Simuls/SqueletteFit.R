a0e1489b844971d8d94d145bef1afb90694a0598
pyoph <paul.guillot2@gmail.com>
2025-06-30 17:58:52 +0200

    ajout code simu début

--- SR/Simuls/SqueletteFit.R
+++ SR/Simuls/SqueletteFit.R
@@ -6,24 +6,64 @@ resDir <- 'FitSim/'
 
 d <- 10
 rList <- 5*(0:10)
-load(paste0('SimParmsGrid-d', d, '.Rdata'))
+#load(paste0('SimParmsGrid-d', d, '.Rdata'))
 rList <- 5*(0:10)
-simNb <- 10
+simNb <- 1
 n <- 1e4
 
-# Fit (k for mu1)
+# Fit (k for mu1) Other parameters are fixed
 for(r in rList){for(k in kList){
   for(sim in 1:simNb){
-    dataFile <- paste0(simDir, 'SimDate-d', d, '-n', n, '-k', k, '-l', 1, '-rho', rho0, '-sim', sim)
-    fitFile <- paste0(resDir, 'FitParms-d', d, '-n', n,  '-k', k, '-l', 1, '-rho', rho0, '-sim', sim)
-    load(dataFile)
+    dataFile <- paste0('SimData-d', d, '-n', n, '-k', k, '-l', 1, '-rho', rho0, '-sim', sim)
+    setwd("C:/Users/Paul GUILLOT/Documents/Simus/DataSim")
+    load(file = dataFile)
     if(!file.exists(fitFile)){
-      fitNaif <- Naif(data)
-      fitUs <- Algo(data)
-      save(fitNaif, fitUs, file=fitFile)
+      fitNaif <- SampleCovOnline(data$Z)
+      fitUsOnline <- StreamingOutlierDetection(data$Z,batch = 1)
+      fitUSStreaming = StreamingOutlierDetection(data$Z,batch = ncol(Z))
+      setwd("C:/Users/Paul GUILLOT/Documents/Simus/Fitsim")
+      save(fitNaif, fitUsOnline, fitUSStreaming,file=fitFile)
     }else{load(fitFile)}
   }
 }}
+
+
+# Fit (l for Sigma1) Other parameters are fixed
+for(r in rList){for(l in lList){
+  for(sim in 1:simNb){
+    dataFile <- paste0('SimData-d', d, '-n', n, '-k', k, '-l', 1, '-rho', rho0, '-sim', sim)
+    fitFile <- paste0('FitParms-d', d, '-n', n,  '-k', k, '-l', 1, '-rho', rho0, '-sim', sim)
+    setwd("C:/Users/Paul GUILLOT/Documents/Simus/DataSim")
+    load(file = dataFile)
+    if(!file.exists(fitFile)){
+      fitNaif <- SampleCovOnline(dataFile$Z)
+      fitUsOnline <- StreamingOutlierDetection(dataFile$Z,batch = 1)
+      fitUSStreaming = StreamingOutlierDetection(dataFile$Z,batch = ncol(Z))
+      setwd("C:/Users/Paul GUILLOT/Documents/Simus/Fitsim")
+      save(fitNaif, fitUsOnline, fitUSStreaming,file=fitFile)
+    }else{load(fitFile)}
+  }
+}}
+
+
+# Fit (rho for Sigma1) Other parameters are fixed
+for(r in rList){for(rho in rho1List){
+  for(sim in 1:simNb){
+    dataFile <- paste0('SimData-d', d, '-n', n, '-k', k, '-l', 1, '-rho', rho, '-sim', sim,".RData")
+    fitFile <- paste0('FitParms-d', d, '-n', n,  '-k', k, '-l', 1, '-rho', rho, '-sim', sim)
+    setwd("C:/Users/Paul GUILLOT/Documents/Simus/DataSim")
+    load(dataFile)
+    if(!file.exists(fitFile)){
+      fitNaif <- SampleCovOnline(data$Z)
+      fitUsOnline <- StreamingOutlierDetection(data$Z,batch = 1)
+      fitUSStreaming = StreamingOutlierDetection(data$Z,batch = ncol(Z))
+      setwd("C:/Users/Paul GUILLOT/Documents/Simus/Fitsim")
+      save(fitNaif, fitUsOnline, fitUSStreaming,file=fitFile)
+    }else{load(fitFile)}
+  }
+}}
+
+
 
 # Fit (all parms)
 for(r in rList){for(k in kList){for(l in lList){for(rho1 in rho1List){
--- SR/Simuls/SqueletteSimul.R
+++ SR/Simuls/SqueletteSimul.R
@@ -28,7 +28,7 @@ for(r in rList){for(k in kList){for(l in lList){for(rho1 in rho1List){
     sigmaSq0 <- (1:d); sigmaSq0 <- sigmaSq0 / mean(sigmaSq0)
     SigmaContamin <- diag(sqrt(sigmaSq0)) %*% toeplitz(rho^(0:(d-1))) %*% diag(sqrt(sigmaSq0))
     data = genererEchantillon(n,d,mu1 = mu0, mu2 = k*m1,Sigma1 = Sigma0,Sigma2 = l*SigmaContamin,p1 =1 - r/100,p2 = r/100,contamin = "moyenne_variance")
-    dataFile <- paste0('SimData-d', d, '-n', n,  '-k', k, '-l', l, '-rho', rho1, '-sim', sim)
+    dataFile <- paste0('SimData-d', d, '-n', n,  '-k', k, '-l', l, '-rho', rho1, '-sim', sim,".RData")
     
     # data <- Simul(d=d, r=r, k=k, rho1=rho1)
      save(data, file=dataFile)
