354c8fcba5de871067a343f4f7878d79fc6ca46b
pyoph <115776656+pyoph@users.noreply.github.com>
2025-11-08 16:08:01 +0100

    Update affiche_resultats.R

--- affiche_resultats.R
+++ affiche_resultats.R
@@ -260,7 +260,7 @@ dev.off()
 
 
 
-#################KL 50####################
+#################KL 25####################
 
 k =k1val[5];l=l1valup1[5];rho1 = rho1val[5];
 
@@ -303,48 +303,66 @@ axis(2,cex.axis = 1.8)
 dev.off()
 
 
-#################KL 100####################
+# Dims
+d <- 10; KLval <- c(0, 1, 5, 10, 25, 50, 100)
 
-k =k1val[6];l=l1valup1[6];rho1 = rho1val[6];
+# Parms
+#load(paste0('~/algosto/SimParmsGrid-d', d, '.Rdata'))
+# l1val <- sort(l1val[which(l1val >= 1)])
+# rho1val <- sort(rho1val[which(rho1val >= rho0)])
+rbind(KLval, k1val, l1valup1, rho1val)
+klNb <- length(KLval)
 
-contParam = ParmsF1(m1, k, l, rho1)
+# Sigma0
+Sigma0inv <- solve(Sigma0)
+Sigma0eig <- eigen(Sigma0)
+Sigma0invHalf <- Sigma0eig$vectors%*%diag(1/sqrt(Sigma0eig$values))%*%t(Sigma0eig$vectors)
+Sigma0half <- Sigma0eig$vectors%*%diag(sqrt(Sigma0eig$values))%*%t(Sigma0eig$vectors)
 
-data = genererEchantillon(n,n,mu1 = mu0,mu2 = contParam$mu1,Sigma1 = Sigma0,Sigma2 = contParam$Sigma1,rate )
+#par(mfrow=c(2, 1))
+B <- 1e6
 
-Z = data$Z
-# Création d'un dataframe pour ggplot
-df <- data.frame(X1 = data$Z[,1], X2 = data$Z[,2], 
-                 Label = factor(data$labelsVrais, levels = c(0, 1)))
-# Définir les couleurs selon le label
-cols <- ifelse(df$Label == 1, "red", "blue")
+klcol = c("black","darkgreen","blue","purple","orange","red","brown")
 
-# Définir les transparences (alpha = 0.5)
-# converti avec adjustcolor()
-cols <- adjustcolor(cols, alpha.f = 0.5)
-
-file = paste0("contaminScen","-k",k,"-l",l,"-rho1",rho1,"-r",rate,".pdf")
-
-pdf(file, width = 8, height = 6)  # Ouvre un device PDF
-
-# Créer le plot principal
-plot(df$X1, df$X2,
-     col = cols,
-     pch = 19,        # points pleins
-     cex = 1.2,       # taille des points
-     xlim = c(a, b),
-     ylim = c(a, b),
-     xlab = "",
-     ylab = "",
-     #main = paste("Rate:", rate, "%, k =", k, ", l =", l, ", rho1 =", rho1),
-     main = "",
-     xaxt = "n", yaxt = "n"  
-)
-
-axis(1,cex.axis = 1.8)
-
-axis(2,cex.axis = 1.8)
+scen_names = c("d","c","b","a")
+  
+# Distribution (log scale)
+U <- matrix(rnorm(d*B), B, d)
+X0 <- rmvnorm(B, mean=mu0, sigma=Sigma0)
+d0 <- rowSums((X0%*%Sigma0invHalf)^2)
+f0 <- density(log10(d0))
+xMin <- 1e-1; xMax <- 1e6
+setwd("~")
+file = "khi2density.pdf"
+pdf(file,width = 8,height = 6)
+plot(f0, xlim=c(log10(xMin), log10(xMax)), ylim=c(0, 2.2),col = klcol[1], main='')  
+# curve(dchisq(x, df=d), from=1e-2, to=max(d0), add=TRUE, lty=2)
+#text(0.98*f0$x[which.max(f0$y)], 1.02*max(f0$y), labels='0', col=klcol[1])
+q0_95 <- quantile(d0, probs = .95)
+abline(v=log10(quantile(d0, probs=.95)), lty=2)
+text(x = log10(quantile(d0, probs=.95))-0.05, y = 2.0, labels = paste0("", round(q0_95, 2)), pos = 4, cex = 0.8)
+compt = 1
+for(kl in 2:(klNb-2)){
+  parms1 <- ParmsF1(m1=m1, k1=k1val[kl], l1=l1valup1[kl], rho1=rho1val[kl])
+  X1 <- rmvnorm(B, mean=parms1$mu, sigma=parms1$Sigma)
+  d1 <- rowSums((X1%*%Sigma0invHalf)^2)
+  f1 <- density(log10(d1))
+  lines(f1, col= klcol[compt+1]) 
+  #q1_95 <- quantile(d1, probs = .95)
+  #abline(v=log10(quantile(d1, probs=.95)), lty=2,col = klcol[(compt+1)])
+  # text(
+  #   x = log10(quantile(d1, probs = .95)) - 0.05,
+  #   y = 2.0 - 0.2 * compt,
+  #   labels = paste0(round(q1_95, 2)),  # texte numérique seulement
+  #   col = klcol[compt + 1],           # couleur appliquée ici
+  #   pos = 4,
+  #   cex = 0.8
+  # )
+  if(kl != 1){
+  text(0.98*f1$x[which.max(f1$y)], 1.02*max(f1$y), labels= scen_names[kl-1], col=klcol[compt+1])}
+compt = compt + 1
+  }
 dev.off()
-
 
 
 #######################################################False positives and false negatives and Sigma error final########################################
